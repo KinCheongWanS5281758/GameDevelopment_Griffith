dac0f08b1b66768d380a80c610b40ac28a2e5caf
KinCheongWanS5281758 <kincheong.wan@griffithuni.edu.au>
2023-10-07 05:31:18 +1000

    Rearrange ICE world: camera, scripts, NavMesh, GUI, Ammo->coins, and more...

--- GroupProject/Assets/Ice_World/Scripts/AmmoPickup.cs
+++ GroupProject/Assets/Ice_World/Scripts/AmmoPickup.cs
@@ -1,18 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AmmoPickup : MonoBehaviour {
+public class AmmoPickup : MonoBehaviour
+{
 
 	public float rotateSpeed = 30.0f;
 
 	// Update is called once per frame
-	void Update () {
-		transform.Rotate(0f ,rotateSpeed * Time.deltaTime, 0f);
+	void Update()
+	{
+		transform.Rotate(0f, rotateSpeed * Time.deltaTime, 0f);
 	}
 
-	void OnTriggerEnter(Collider col) {
-		if (col.gameObject.tag == "Player") {
-			col.gameObject.SendMessage("ApplyAmmoPickup");
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.gameObject.SendMessage("ApplyCoinPickup");
 			Destroy(gameObject);
 		}
 	}
